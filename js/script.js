24d4b7de26cccd174a2e535e18e72815af5b7670
Sebastian Szaładyga <sebastian.szaladyga@gmail.com>
2022-12-24 12:40:28 +0100

    corrected function

--- js/script.js
+++ js/script.js
@@ -1,10 +1,20 @@
-console.log ("Cześć, przedstawione zadanie posiada elementy satyryczne")
-
-let button = document.querySelector (".js-button");
-let body = document.querySelector (".js-body");
-let themeName= document.querySelector (".js-themeName");
-
-button.addEventListener ("click", () => {
-        body.classList.toggle ("body--dark");
+{
+    const welcome = () => {
+        console.log("Cześć, przedstawione zadanie posiada elementy satyryczne")
+    };
+    
+    const toggleBackground = () => {
+        const body = document.querySelector(".js-body");
+        const themeName = document.querySelector(".js-themeName");
+        body.classList.toggle("body--dark");
         themeName.innerText = body.classList.contains("body--dark") ? "jasny" : "ciemny"
-    });
+    };
+    
+    const init = () => {
+        const button = document.querySelector(".js-button");
+        button.addEventListener("click", toggleBackground);
+    }
+
+    init ();
+    welcome();
+}
